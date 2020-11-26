8ae8d8b402b4784420b651cbf6933acb47db93a1
shubhamsugara22 <shubhamsugara22@gmail.com>
2020-11-26 20:38:36 +0530

    bmicalci

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -6,9 +6,31 @@ const app = express();
 app.use(bodyParser.urlencoded({extended:true}))
 
 const PORT  =  3000;
+//get ofr calculator and home page
+
 app.get( "/" ,function(request ,response ){
     response.sendFile(__dirname + "/index.html")
 });
+
+// get request for BMi calculator
+
+app.get("/bmicalculator",function(request,response){
+    response.sendFile(__dirname +"/bmicalculator.html")
+});
+
+// post for bmi calculator
+
+app.post("/bmicalculator",function(request, response){
+
+    var weight  = parseFloat(request.body.weight);
+    var height  = parseFloat(request.body.height);
+
+    var bmi  = weight / (weight * height) ;
+
+    response.send("your BMI is  "+ bmi);
+});
+
+// post for calculator
 
 app.post("/", function(request,response){
     var num1  = Number(request.body.num1) ;
